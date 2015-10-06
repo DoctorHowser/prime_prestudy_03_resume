e54baa09a92067f80b8a6f88e6ed34b689f68e6e
Dane Smith <doctorhowser@gmail.com>
2015-10-06 13:45:22 -0500

    Add button, functionality

--- application.js
+++ application.js
@@ -1,3 +1,8 @@
 $("document").ready(function(){
-	
+	$('#container').fadeOut();
+	$('#verify').fadeIn();
+	$('#verify').on('click', function (){
+		$(this).fadeOut();
+		$(this).parent().parent().find('#container').fadeIn();
+	})
 })
