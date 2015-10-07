7356531dbb1179c2ba62ad9eb47b82582d4e18cf
Dane Smith <doctorhowser@gmail.com>
2015-10-07 18:48:18 -0500

    Add complexity to JQ, fix HTML and CSS to reflect needs

--- application.js
+++ application.js
@@ -1,7 +1,14 @@
 $("document").ready(function(){
-	$('#verify').fadeIn();
-	$('#verify').on('click', function (){
-		$(this).fadeOut();
+	var clicks = 0;
+	$('.verify').fadeIn();
+	$('.verify').on('click', 'button', function (){
+		clicks++;
+		$('body').append("<div class='verify'></div>")
+		var el = $('body').children().last();
+		el.append("<p >Revealing...try again...</p>");
+		if (clicks === 3) {
+			$(".verify").fadeOut();
 		$(this).closest('body').find('#container').fadeIn();
-	})
-})
+		};		
+	});
+});
